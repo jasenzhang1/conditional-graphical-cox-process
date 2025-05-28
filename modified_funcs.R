9915cabbc95004b8b6e6baef2b5074aa060b448e
JASEN ZHANG <jasenzz@n1826.hoffman2.idre.ucla.edu>
2025-05-28 15:46:57 -0700

    test

--- modified_funcs.R
+++ modified_funcs.R
@@ -676,13 +676,11 @@ get_rho_diag_pp_troubleshoot <- function(data_all=NULL,patient_sel=NULL,feature_
     Sigma_ii = cross_prod(X=xx,NN=NN,remove_diag=remove_diag)
 
     if(i %in% c(30, 91, 101)){
-      print(xx)
-      print(NN)
       print(Sigma_ii)
     }    
         
     ## pca with svd
-    eigen_res = eigen(Sigma_ii)
+    eigen_res = eigen(Sigma_ii, symmetric = T)
     eigen_res$values[ eigen_res$values<0] = 0 
     positiveInd = eigen_res$values >= 0
     d = eigen_res$values[positiveInd]
