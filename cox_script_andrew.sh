967765bba2f72d08bc189f293d9ab0702c3958a0
jasen <jasenzhang@g.ucla.edu>
2025-05-27 23:49:12 -0700

    cox scripts

---
+++ cox_script_andrew.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Run R script and write output to output.txt
+Rscript Alzheimers_run_qrsh.R > cox_output.txt 2>&1
+
---
+++ cox_script_weekly_movement.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Run R script and write output to output.txt
+Rscript Alzheimers_run_weekly_movement.R > output_weekly_movement.txt 2>&1
+
