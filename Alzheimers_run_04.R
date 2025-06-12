268c38b276155b8477b8f1816c725ca4cc34f4f7
JASEN ZHANG <jasenzz@n6046.hoffman2.idre.ucla.edu>
2025-06-11 23:41:36 -0700

    rerun task 4

--- Alzheimers_run_04.R
+++ Alzheimers_run_04.R
@@ -13,19 +13,23 @@ library(dplyr)
 
 # 1) Open 99_Create_Spiketrain_Dataset and load the data_df file
 
+# 1a) Parameters that don't change
+
 IDs <- c('346', '351', '366', '361', '362', '368')  # mouse ID
 ID2 <- c('Tau1', 'Tau2', 'Tau3', 'WT1', 'WT2', 'WT3') # our name 
 
 num_neurons <- c(169, 250, 240, 249, 235, 294)
 names(num_neurons) <- ID2
 
+# 1b) Parameters that change, double check!!!
+
+task_num <- '04'
 
 movements <- c(0, 1, 2)
+VR <- 0
 
 ews <- 1:4
 ew_symb <- 'e'
-
-VR <- 0
 
 tn_symb <- 't'
 tns <- 10  # replicates or timescale
@@ -42,7 +46,7 @@ for(ew in ews){ # for each epoch/week
     
     for(movement in movements){ # for each movement
         
-        data_root <- "spike_data/task_04/"  # CHANGE THIS # 1
+        data_root <- paste0("spike_data/task_", task_num, '/') # spike_data/task_04/
         
         setting_ID <- paste0(ew_symb, ew, '_m', movement, 'vr', VR, '_', tn_symb, tns) # e1_m0vr0_t20
         setting_ID2 <- paste0(setting_ID, '_me', min_edges)                            # e1_m0vr0_t20_me1
@@ -67,14 +71,13 @@ for(ew in ews){ # for each epoch/week
             
             load(file_names[i]) # data_df2
             
-            time_scale <- data_df2[[2]]
+            time_scale <- tns
+            num_replicates <- unique(data_df2$subject_num) %>% length()
             print(paste0('timescale of each replicate: ', time_scale, ' secs'))
-            
+            print(paste0('number of replicates: ', num_replicates))
             
             # which neurons fire way too little and must be discarded (under 100 total firings = discard)
             
-        
-            data_df2 <- as.data.table(data_df2[[1]])
             
             data_df3 <- data_df2[, if (.N >= 100) .SD, by = feature_id]
             
@@ -249,15 +252,16 @@ for(ew in ews){ # for each epoch/week
             
 
             graph_all[['time_scale']] <- time_scale
-             
+            graph_all[['num_replicates']] <- num_replicates
+            
             ### save results 
             
-            save_dir <- 'results_alzheimers/task_02/'  # CHANGE THIS # 2
+            save_dir <- paste0('results_alzheimers/task_', task_num, '/')  # results_alzheimers/task_04/
             if (!dir.exists(save_dir)) {
                 dir.create(save_dir)
             }               
             
-            save_dir <- paste(save_dir, '/', sep = setting_ID2)                       #../result_simu/e1_m2vr0_n400_me1/
+            save_dir <- paste0(save_dir, setting_ID2, '/')                       #../task_04/e1_m2vr0_t10_me1/
             
             # if the folder doesn't exist, create it 
             if (!dir.exists(save_dir)) {
@@ -278,7 +282,6 @@ for(ew in ews){ # for each epoch/week
             print(paste('minutes taken: ', elapsed, sep = ''))
             
         }
-        
     }
 }
 print('-----------------------------------------------------------')
