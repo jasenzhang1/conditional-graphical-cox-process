0b83f40458f688ddb78a061a9d5d90aac78a831b
JASEN ZHANG <jasenzz@n1828.hoffman2.idre.ucla.edu>
2025-08-27 17:21:58 -0700

    temp

--- Conditional_LGCP/main_simulation_unpacking.R
+++ Conditional_LGCP/main_simulation_unpacking.R
@@ -3,14 +3,21 @@ library(grid)
 library(gridExtra)
 
 
+# 1) graphs that don't change over time 
+# 2) graphs that change over time, but values below adjacency threshold are 0
+# 3) ...
 
-load('simu_results_banded_v1/n_100.RData')
+# 10) continuous covariates = (week, velocity, VR)
+
+
+load('simu_results_banded_v1/n_3000.RData')
+
 
 grid.newpage()
-grid.draw(graph_results_i$estimated_graphs_part_2$`4`$g_112)
+grid.draw(graph_results_i$estimated_graphs_part_2$`3`$g_112)
 
 
 grid.newpage()
 pdf('temp2.pdf', width = 20, height = 14)
-grid.draw(graph_results_i$estimated_graphs_part_2$g_113)
+grid.draw(graph_results_i$estimated_graphs_part_2$`4`$g_84)
 dev.off()
