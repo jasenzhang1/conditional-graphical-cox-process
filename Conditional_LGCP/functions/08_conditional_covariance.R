9ae986403d66cae7a402ba763e698b6bc483770d
JASEN ZHANG <jasenzz@n1828.hoffman2.idre.ucla.edu>
2025-09-09 23:32:13 -0700

    evaluate regression v3 init

--- Conditional_LGCP/functions/08_conditional_covariance.R
+++ Conditional_LGCP/functions/08_conditional_covariance.R
@@ -160,8 +160,8 @@ evaluate_regression_at_query_diag_only <- function(M_hat, Y_continuous_stratum, 
 }
 
 # in use
-evaluate_regression_at_query_v2 <- function(M_hat, Y_continuous_stratum, query_y_c, 
-                                         eigenfunctions, gamma_c, p) {
+evaluate_regression_at_query_v2 <- function(M_hat, Y_continuous_stratum, query_y_c,
+                                            eigenfunctions, gamma_c, p) {
   
   
   # ----------------------------------------------------------------------------
@@ -247,3 +247,92 @@ evaluate_regression_at_query_v2 <- function(M_hat, Y_continuous_stratum, query_y
   return(V_conditional)
 }
 
+# JASA Paper
+# We define our regresion operator as a function of Y_c
+
+evaluate_regression_at_query_v3 <- function(M_hat, y_c_strata, query_y_c,
+                                            eigenfunctions, gamma_c, p) {
+  
+  
+  # ----------------------------------------------------------------------------
+  #
+  # GOAL: evaluate regression at query
+  #
+  #       do steps 6, 7, and 8 all together
+  #       in the JASA method, we include y_c's effect on the cross-covariance term immediately
+  #       so we don't apply the y_c weight here
+  #
+  #
+  # Input: 
+  #
+  # - M_hat                  (p^2 length list where each entry is 'i_j') 
+  # - y_c_strata             (n_stratum x q_c matrix)
+  # - query_y_c              (q_c dim vector)
+  # - eigenfunctions         (list of p matrices, each of which is m x d_i)
+  # - gamma_c                (number)
+  # - p                      (integer)
+  #
+  #
+  # Output: 
+  #
+  # - V_conditional (list of length p^2, each element is m x m matrix)
+  #
+  #
+  # ----------------------------------------------------------------------------
+  
+  components <- sapply(eigenfunctions, ncol) %>% unlist()
+  max_components <- max(components)            # d
+  n_time <- nrow(eigenfunctions[[1]])          # m
+  n_stratum <- nrow(y_c_strata)                # n 
+  
+  
+  V_conditional <- list()
+  
+  for (i in 1:p) {
+    for (j in i:p) {
+      key <- paste(i, j, sep="_")
+      
+      # Construct conditional covariance operator: m x m matrix
+      V_cond_ij <- matrix(0, nrow=n_time, ncol=n_time)
+      
+      # recall M_hat[[key]] = n x d x d
+      # flatten it to be n x (d^2)
+      M_hat_ij_flat <- matrix(M_hat[[key]], nrow = n_stratum, ncol = max_components^2) 
+      M_at_query <- apply(M_hat_ij_flat, 2, sum) # (comps^2) length vector
+      
+      
+      
+      # Split columns into vectors
+      A_cols <- asplit(eigenfunctions[[i]], 2)  # list of m vectors (length 19)
+      B_cols <- asplit(eigenfunctions[[j]], 2)  # list of n vectors
+      
+      
+      # append A_cols and B_cols with 0's so that they have a list of (comps) vectors
+      
+      while(length(A_cols) < max_components){
+        A_cols[[length(A_cols) + 1]] <- rep(0, n_time)
+      } 
+      
+      while(length(B_cols) < max_components){
+        B_cols[[length(B_cols) + 1]] <- rep(0, n_time)
+      }       
+      
+      # Compute all d × d outer products using tcrossprod
+      # Result: list of d * d matrices
+      
+      # BE CAREFUL!!! B_cols needs to be repeated for the amount of components of i
+      #               A_cols needs to be repeated for the amount of components of j
+      outer_products <- Map(tcrossprod,
+                            rep(A_cols, times = max_components),
+                            rep(B_cols,  each = max_components))
+      
+      # Weighted sum of all outer products
+      result <- Reduce(`+`, Map(`*`, M_at_query, outer_products))
+      
+      V_conditional[[key]] <- result
+      
+    }
+  }
+  
+  return(V_conditional)
+}
