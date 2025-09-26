92157312db59379475d193c8d508b9490c5458a6
JASEN ZHANG <jasenzz@n1826.hoffman2.idre.ucla.edu>
2025-09-26 15:40:35 -0700

    ready to run CPGM!

--- Conditional_LGCP/main_simulation.R
+++ Conditional_LGCP/main_simulation.R
@@ -51,7 +51,7 @@ query_y_cs = matrix(0:8/8)               # query y_values
 
 # 6) sample size and # of processes
 ns <- c(100, 300, 1000, 3000, 10000)     # Sample size (n)
-ns <- c(200)
+ns <- c(100, 200, 500, 1000)
 n_large <- max(ns)
 
 p = 10                                   # Number of processes (p)  
