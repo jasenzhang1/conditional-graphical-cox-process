6c84c255897a81d1f15aa67a6a2befa3689b0d93
JASEN ZHANG <jasenzz@n1826.hoffman2.idre.ucla.edu>
2025-10-03 11:37:59 -0700

    added inner_1 argument

--- Conditional_LGCP/functions/04_eigendecomposition.R
+++ Conditional_LGCP/functions/04_eigendecomposition.R
@@ -73,6 +73,8 @@ prep_eigendecomposition_ii <- function(G_hat, p){
   #
   # GOAL: restructure G_ij so that it can be fed into `compute_eigendecomposition_ii`
   #
+  # - only keep the i_i values from the list
+  #
   #
   # input:
   #
@@ -102,7 +104,7 @@ prep_eigendecomposition_ii <- function(G_hat, p){
   return(G_mat)
 }
 
-compute_eigendecomposition_ii <- function(G_hat, var_explained = 0.999) {
+compute_eigendecomposition_ii <- function(G_hat, inner_1, var_explained = 0.999) {
   
   # ----------------------------------------------------------------------------
   #
@@ -114,78 +116,9 @@ compute_eigendecomposition_ii <- function(G_hat, var_explained = 0.999) {
   # Input: 
   # 
   # - G_hat (m x m array) (m x m x p)
-  # 
-  # 
-  # Output: 
-  #
-  # - eigenvalues    (list of p vectors)
-  # - eigenfunctions (list of p matrices)
-  # - n_dims         (list of p numbers denoting d_i)
-  #
-  # ----------------------------------------------------------------------------
-  
-  p <- dim(G_hat)[3]
-  n_time <- dim(G_hat)[1]  # m
-  
-  eigenvalues <- list()
-  eigenfunctions <- list()
-  n_dims <- list()
-  
-  for (i in 1:p) {
-    # Extract marginal covariance matrix: m x m
-    G_ii <- G_hat[, , i]  
-    
-    # Ensure symmetry for numerical stability
-    G_ii <- (G_ii + t(G_ii)) / 2
-    
-    # Compute eigendecomposition
-    # eigen() returns: values (m x 1), vectors (m x m)
-    eigen_result <- eigen(G_ii, symmetric=TRUE)
-    lambdas <- eigen_result$values  # m x 1 vector
-    lambdas2 <- lambdas
-    lambdas2[lambdas2 < 0] <- 0
-    
-    cum_var <- cumsum(lambdas2) / sum(lambdas2)
-    d_i <- which(cum_var > var_explained)[1]
-    etas <- eigen_result$vectors    # m x m matrix
-    
-    
-    
-    # Keep only top d components
-    
-    eigenvalues[[i]] <- lambdas[1:d_i]        # d x 1 vector
-    
-    if(d_i == 1){
-      eigenfunctions[[i]] <- matrix(etas[, 1:d_i], nrow = n_time)
-    } else{
-      eigenfunctions[[i]] <- etas[, 1:d_i]      # m x d matrix
-    }
-    
-    n_dims[[i]] <- d_i
-  }
-  
-  # keep track of how many components are needed to explain 90%
-  
-  return(list(eigenvalues = eigenvalues, eigenfunctions = eigenfunctions, n_dims = n_dims))
-}
-
-compute_eigendecomposition_ii_v2 <- function(G_hat, var_explained = 0.999) {
-  
-  # ----------------------------------------------------------------------------
-  #
-  #
-  # GOAL: from G_{i,i}(s,t), estimate the eigendecomposition
-  #
-  # - note that we only have G_{i, i} entries 
-  #
-  # - 9/26/2025 
-  #   - v2:
-  #   - normalize eigenvectors so that instead of v^\top v = 1, Delta * v^\top v = 1
-  #
-  # 
-  # Input: 
-  # 
-  # - G_hat (m x m array) (m x m x p)
+  # - inner_1       (boolean)  does the inner product of eta^\top eta = 1? 
+  #                            If false, Delta * eta^\top \eta = 1
+  # - var_explained (percentage)
   # 
   # 
   # Output: 
@@ -198,7 +131,7 @@ compute_eigendecomposition_ii_v2 <- function(G_hat, var_explained = 0.999) {
   
   p <- dim(G_hat)[3]
   m <- dim(G_hat)[1]
-  Delta <- 1 / m
+  Delta <- 1/m
   
   eigenvalues <- list()
   eigenfunctions <- list()
@@ -207,8 +140,12 @@ compute_eigendecomposition_ii_v2 <- function(G_hat, var_explained = 0.999) {
   for (i in 1:p) {
     # Extract marginal covariance matrix: m x m
     
-    # V2: WE NORMALIZE BY DIVIDING BY M FIRST
-    G_ii <- G_hat[, , i]  / m
+    if(inner_1){
+      G_ii <- G_hat[, , i]  # / m
+    } else{
+      G_ii <- G_hat[, , i]  # / m
+    }
+    
     
     # Ensure symmetry for numerical stability
     G_ii <- (G_ii + t(G_ii)) / 2
@@ -230,10 +167,18 @@ compute_eigendecomposition_ii_v2 <- function(G_hat, var_explained = 0.999) {
     
     eigenvalues[[i]] <- lambdas[1:d_i]        # d x 1 vector
     
-    if(d_i == 1){
-      eigenfunctions[[i]] <- matrix(etas[, 1:d_i], nrow = m) / sqrt(Delta)
+    if(inner_1){
+      if(d_i == 1){
+        eigenfunctions[[i]] <- matrix(etas[, 1:d_i], nrow = m)
+      } else{
+        eigenfunctions[[i]] <- etas[, 1:d_i]      # m x d matrix
+      }
     } else{
-      eigenfunctions[[i]] <- etas[, 1:d_i] / sqrt(Delta)     # m x d matrix
+      if(d_i == 1){
+        eigenfunctions[[i]] <- matrix(etas[, 1:d_i], nrow = m) # / sqrt(Delta)
+      } else{
+        eigenfunctions[[i]] <- etas[, 1:d_i] # / sqrt(Delta)     # m x d matrix
+      }      
     }
     
     n_dims[[i]] <- d_i
