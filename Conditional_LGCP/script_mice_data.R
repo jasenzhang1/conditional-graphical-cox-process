b55807ed801138be8a9828e245f09dd3a61fd720
JASEN ZHANG <jasenzz@n1827.hoffman2.idre.ucla.edu>
2025-10-13 13:54:00 -0700

    generate mice data script init

---
+++ Conditional_LGCP/script_mice_data.R
@@ -0,0 +1,48 @@
+
+# script to create mice replicate (y_c, y_d) data for LGCP estimation
+
+t0 <- Sys.time()
+
+args <- commandArgs(trailingOnly = TRUE)
+
+
+ID <- args[1]
+time_scale <- as.numeric(args[2])
+
+
+
+source('functions/40_mice_data_functions.R')
+data_dir <- 'data/'
+
+
+
+weekly_dataset <- load_realigned_dataset(data_dir)[[1]]
+neuron_df <- load_brain_region_dataset(data_dir)[[1]]
+
+ID2 <- as.character(neuron_df$ID2[which(neuron_df$Mouse == ID)][1])
+
+discrete_covariates <- c('movement', 'VR')
+continuous_covariates <- c('week', 'timestamp')
+
+save_dir <- "data/with_ts"
+if(!dir.exists(save_dir)){
+  dir.create(save_dir, recursive = TRUE)
+}
+
+
+    
+# obtain data
+t_start <- Sys.time()
+LGCP_data <- get_spiketrain_dataset_conditional_LGCP(ID, time_scale, discrete_covariates, continuous_covariates, weekly_dataset, neuron_df)
+
+# prepare to save
+file_name <- paste0(save_dir, '/', ID2, '_t', time_scale, '_data.rda')
+
+# save 
+save(LGCP_data, file = file_name)
+
+# report time taken
+t_end <- Sys.time()
+t_elapsed <- difftime(t_end, t_start, units = 'mins') %>% as.numeric() %>% round(2)
+print(paste0(ID2, '_t', time_scale, ' finished in ', t_elapsed, ' mins'))
+
---
+++ Conditional_LGCP/scripts/generate_mice_data.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Loop over dataset indices
+
+cd "$(dirname "$0")/.."   # go one level up (from /scripts to /)
+
+IDs=(346 351 366 361 362 368)
+time_scales=(1 2 5 10)
+max_jobs=12
+
+mkdir -p script_outputs
+
+outfile="script_outputs/generate_mice_data.txt"
+echo "Logging to: $outfile"
+
+
+for ID in "${IDs[@]}"; do
+  for time_scale in "${time_scales[@]}"; do
+  
+    # Wait until fewer than max_jobs are running
+    while (( $(jobs -r | wc -l) >= max_jobs )); do
+      sleep 1
+    done  
+  
+    echo "Generating dataset: mouse=$ID, timescale=$time_scale"
+    
+    # Run Rscript in background and measure time
+    (
+      start_time=$(date +%s)
+      Rscript script_mice_data.R $ID $time_scale >> "$outfile" 2>&1
+      end_time=$(date +%s)
+      elapsed=$((end_time - start_time))
+      minutes=$((elapsed / 60))
+      seconds=$((elapsed % 60))
+      echo "Mouse $ID, timescale $time_scale finished in ${minutes}m ${seconds}s" >> "$outfile"
+    ) &  
+    
+  done
+done
+
+wait
+
+echo "Finished generating all mice datasets."
+
