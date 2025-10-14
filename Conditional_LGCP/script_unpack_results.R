2c97e85a38083e0b844d194e9c9467e2b0157017
JASEN ZHANG <jasenzz@n1144.hoffman2.idre.ucla.edu>
2025-10-13 17:47:09 -0700

    also plotting prec mat comparisons

--- Conditional_LGCP/script_unpack_results.R
+++ Conditional_LGCP/script_unpack_results.R
@@ -4,6 +4,7 @@ library(gridExtra)
 library(dplyr)
 source('functions/28_Simulation_Visualization.R')
 source('functions/28b_Simulation_Visualization_2.R')
+source('functions/28c_Grand_Visualizations.R')
 source('functions/00d_debugging.R')
 
 
@@ -29,3 +30,20 @@ write.csv(metrics_summary$metric_table, paste0(results_folder, "/metrics_summary
 pdf(paste0(results_folder, "/metrics_summary.pdf"), width = 8, height = 6)  # open PDF file
 grid.arrange(metrics_summary$metric_graph)                                 # draw the grob/layout
 dev.off()                                                                  # close the file
+
+
+# 2) ground truth vs estimated p x p matrix
+
+max_n <- max(get_ns(results_folder))
+
+n_y_c_query <- length(get_y_c_query(folder_name, max_n))
+
+prec_mat_comparison <- visualize_prec_mat_over_time(results_folder, max_n)
+pdf(paste0(results_folder, "/prec_mat_over_time.pdf"), width = 3 * (n_y_c_query-2), height = 3)  # open PDF file
+grid.arrange(prec_mat_comparison)                                                            # draw the grob/layout
+dev.off()  
+
+  
+  
+  
+  
