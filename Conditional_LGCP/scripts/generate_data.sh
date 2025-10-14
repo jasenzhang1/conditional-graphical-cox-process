76beaf7502b4371253bb59dc8f140b1063f7c683
JASEN ZHANG <jasenzz@n1945.hoffman2.idre.ucla.edu>
2025-10-14 13:27:53 -0700

    ready to generate single_j2

--- Conditional_LGCP/scripts/generate_data.sh
+++ Conditional_LGCP/scripts/generate_data.sh
@@ -5,14 +5,15 @@
 cd "$(dirname "$0")/.."   # go one level up (from /scripts to /)
 
 adj_type_params=(
-  "indep_2 0 1"
-  "single_c2 0 1 0.7"
-  "single_v2 0 1 0.7"
+  #"indep_2 0 1"
+  #"single_c2 0 1 0.7"
+  #"single_v2 0 1 0.7"
+  "single_j2 0 1 0.5 0.3 0.7"
   #"banded_c2 0 1 0.3"
   #"banded_trig2 0 1 0.3"
   #"sparse_v2 0 1 2 0.3 -1 2 0.01"
 )
-n=100
+n=2000
 
 mkdir -p script_outputs
 
