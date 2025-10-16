b31546cb073f924bf257bc6cd6c29d24a75a97fd
JASEN ZHANG <jasenzz@n1826.hoffman2.idre.ucla.edu>
2025-10-15 17:41:39 -0700

    beginning to plot all diagnostics for all y_c_query

--- Conditional_LGCP/functions/28c_Grand_Visualizations.R
+++ Conditional_LGCP/functions/28c_Grand_Visualizations.R
@@ -1,6 +1,11 @@
-
+source('functions/28_Simulation_Visualization.R')
 source('functions/28z_Visualization_helpers.R')
 source('functions/00c_block_matrix_arrange.R')
+
+library(ggplot2)
+library(patchwork)
+library(grid)
+library(gridExtra)
 
 # these functions open an entire dataset and search through all query_id 
 
@@ -88,16 +93,86 @@ visualize_over_time <- function(folder_name, n, est_only, graph_id, m_est, m, i,
   # 1) load
   
   graph_results_i <- get_file_name_and_load(folder_name, n) 
+  graph_results_i <- old_to_new_graph_results_i(graph_results_i)
+  
+  # prep
+  arr_mat <- matrix(1:6, nrow = 2, byrow = F)
+  arr_mat_8 <- matrix(1:8, nrow = 2, byrow = F)
+  arr_mat_10 <- matrix(1:10, nrow = 2, byrow = F)  
+  
+  
+  graphs <- list()
+  
+  y_c_names <- names(graph_results_i$kernel_params_i) %>% as.numeric() %>% round(2)
   
   # 2) figure out which steps have unique results for y_c_query and which steps are constant
   
-  step_const <- names(graph_results_i$estimated_graphs_part_1)
-  step_vary <- names(graph_results_i$estimated_graphs_part_2[[1]])
-  y_c_query <- names(graph_results_i$estimated_graphs_part_2)
+  if('11' %in% graph_ids){
+    #   - X_k_est                   (p x m_est   x n)
+    #   - X_k_truth                 (p x m_truth x n)
+    #   - X_k_coarse_truth          (p x m_est   x n)
+    #   - X_k_both_truth            (p x m_both  x n)
+    #   - Lambda_k_truth            (p x m_truth x n)
+    #   - Lambda_k_coarse_truth     (p x m_est   x n)
+    
+    step_1 <- graph_results_i$step_1
+    
+    mu_t <- graph_results_i$kernel_params_i[[1]]$base_mean
+    mu_t_est <- graph_results_i$kernel_params_i[[1]]$base_mean_est
+    mu_t_both <- graph_results_i$kernel_params_i[[1]]$base_mean_both
+    
+    graphs[['g_11']] <- grid.arrange(visualize_log_intensity(step_1[[1]][1:5,,1],   time_grid_est,  'Estimate', mu_t_est ),
+                                     visualize_log_intensity(step_1[[3]][1:5,,1],   time_grid_est,  'Coarser Truth', mu_t_est ),
+                                     visualize_log_intensity(step_1[[2]][1:5,,1],   time_grid,      'Finer Truth', mu_t     ),
+                                     visualize_log_intensity(step_1[[4]][1:5,,1],   time_grid_both, 'Combined Truth', mu_t_both),
+                                     textGrob("0. Log Intensity\n of first 5 processes\nof subject 1", gp = gpar(fontsize = 14)),
+                                     layout_matrix = arr_mat) 
+  }  
   
-  if('81' %in% graph_ids & 'step_8' %in% step_vary){
-    est_graphs <- lapply(graph_results_i$estimated_graphs_part_2, function(x) extract_block_structure_ij(x$step_8$V_cond_est_full, m_est, i, j))
+  if('81' %in% graph_ids & 'step_8' %in% names(graph_results_i)){
+    est_graphs <- lapply(graph_results_i$step_8, function(x) extract_block_structure_ij(x$step_8$V_cond_est_full, m_est, i, j))
   }
+  
+  if('113' %in% graph_ids){
+    
+    all_plots <- list()
+    
+    for (i in seq_along(graph_results_i$step_12)) {
+      for (j in seq_along(graph_results_i$step_12[[i]])) {
+        
+        roc_entry       <- graph_results_i$step_12[[i]][[j]]
+        roc_df          <- roc_entry$roc_df
+        ideal_spec      <- roc_entry$specificity
+        ideal_sens      <- roc_entry$sensitivity
+        ideal_threshold <- roc_entry$threshold
+        auc_value       <- roc_entry$auc
+        
+        p <- ggplot(roc_df, aes(x = FPR, y = TPR)) +
+          geom_step(direction = "vh", color = "blue", size = 1) +
+          geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "grey") +
+          labs(
+            title = paste("ROC:", i, "-", j),
+            x = "False Positive Rate", y = "True Positive Rate"
+          ) +
+          annotate("point", x = 1 - ideal_spec, y = ideal_sens, color = "red", size = 3) +
+          annotate("text", x = 1 - ideal_spec, y = ideal_sens,
+                   label = paste0("thr=", round(ideal_threshold, 3)),
+                   hjust = -0.1, vjust = -0.5, color = "red") +
+          annotate("text", x = 0.6, y = 0.2,
+                   label = paste0("AUC=", round(auc_value, 3)),
+                   color = "darkgreen", size = 5) +
+          theme_minimal(base_size = 10)
+        
+        all_plots[[length(all_plots) + 1]] <- p
+      }
+    }
+    
+    # arrange all 70 plots in a 10x7 grid
+    graphs[['113']] <- wrap_plots(all_plots, ncol = 7, nrow = 10) 
+ 
+  }
+  
+  return(graphs)
   
   
 }
