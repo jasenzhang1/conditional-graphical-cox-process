52d4f55201649c01b18f9a753faa3298ca05a092
JASEN ZHANG <jasenzz@n1831.hoffman2.idre.ucla.edu>
2025-11-03 17:24:09 -0800

    generating y_c with last digit

--- Conditional_LGCP/functions/22_generate_random_variables.R
+++ Conditional_LGCP/functions/22_generate_random_variables.R
@@ -1,6 +1,8 @@
 
 library(mvtnorm)
 
+# all functions to generate y_c
+
 # 1) generate continuous and discrete RV's
 # 2) generate only continuous RV's because we assume 1 strata
 # 3) generate continuous RV's that reflect week; assume 1 strata
@@ -157,7 +159,6 @@ generate_conditioning_variables_one_strata_weeks <- function(n, week_vec, seed =
   ))
 }
 
-
 generate_y_c_adj_type <- function(n, adj_type, params, seed = NULL){
   
   # ----------------------------------------------------------------------------
@@ -169,7 +170,7 @@ generate_y_c_adj_type <- function(n, adj_type, params, seed = NULL){
   # inputs:
   #
   # - n               (integer)          sample size
-  # - adj_type        (string)           precision matrix adjacency type
+  # - adj_type        (string)           precision matrix adjacency type (e.g. single_c2)
   # - params          (vector)           associated vector of parameters of the adjacency type
   # - seed            (integer)          reproducibility seed 
   #
@@ -185,17 +186,13 @@ generate_y_c_adj_type <- function(n, adj_type, params, seed = NULL){
     set.seed(seed)
   }
   
-  if(!adj_type %in% c('indep_c2',
-                      'single_c2', 'single_v2', 'single_j2',
-                      'banded_trig1', 'banded_trig2', 
-                      'banded_c0', 'banded_c1', 'banded_c2', 'banded_c3',
-                      'sparse_v1', 'sparse_v2')){
-    stop('adj_type is not supported')
-  }
+  # extract last two letters
   
-  if(adj_type == 'banded_c0'){
+  y_c_type <- substring(adj_type, nchar(adj_type))
+  
+  if(y_c_type == '0'){
     
-    # params = c(value)
+    # params[1] = value
     # all covariates are the same
     
     Y_continuous <- matrix(rep(params[1], n), nrow = n, ncol = 1)
@@ -204,7 +201,7 @@ generate_y_c_adj_type <- function(n, adj_type, params, seed = NULL){
     
   }   
   
-  if(adj_type %in% c('banded_trig1', 'banded_c1', 'sparse_v1')){
+  if(y_c_type == '1'){
     
     # params[1] = min
     # params[2] = max
@@ -217,7 +214,7 @@ generate_y_c_adj_type <- function(n, adj_type, params, seed = NULL){
     
   }
   
-  if(adj_type %in% c('indep_c2', 'single_c2', 'single_v2', 'single_j2', 'banded_trig2', 'banded_c2', 'sparse_v2')){
+  if(y_c_type == '2'){
     
     # params[1] = min
     # params[2] = max
@@ -229,7 +226,7 @@ generate_y_c_adj_type <- function(n, adj_type, params, seed = NULL){
     
   }  
   
-  if(adj_type %in% c('banded_c3')){
+  if(y_c_type == '3'){
     
     # params[1] = min
     # params[2] = max
