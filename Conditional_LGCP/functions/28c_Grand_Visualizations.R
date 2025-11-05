f4e419e63ed07dfb96eeb6b01950d0904b8d6513
JASEN ZHANG <jasenzz@n6036.hoffman2.idre.ucla.edu>
2025-11-05 14:55:16 -0800

    added full parameter

--- Conditional_LGCP/functions/28c_Grand_Visualizations.R
+++ Conditional_LGCP/functions/28c_Grand_Visualizations.R
@@ -68,9 +68,33 @@ visualize_prec_mat_over_time <- function(folder_name, n){
 }
 
 
+# helper 
+rearrange_plots <- function(g_list){
+  
+  # g_list is a list of lists
+  # - first layer is for each y_c
+  # - second layer is for (truth theory, est etc)
+  
+  
+  n_time <- length(g_list)
+  n_graphs <- length(g_list[[1]])
+  N <- n_time * n_graphs
+  
+  # unlist the plots, reorganize them
+  plots <- unlist(g_list, recursive = FALSE)
+  indices <- as.vector(sapply(1:n_graphs, function(r) r + (0:(n_time-1))*n_graphs))
+  plots_colwise <- plots[indices]
+  
+  
+  # plot them
+  arranged_plot <- do.call(grid.arrange, c(plots_colwise, nrow = n_graphs, ncol = n_time))  
+  
+  return(arranged_plot)
+}
+
 # everything over all query_id
 
-visualize_over_time <- function(folder_name, n, est_only, graph_id, m_est, m, i, j){
+visualize_over_time <- function(graph_results_i, graph_ids, i, j, full = T){
   
   
   # ----------------------------------------------------------------------------
@@ -81,27 +105,48 @@ visualize_over_time <- function(folder_name, n, est_only, graph_id, m_est, m, i,
   # 
   # input: 
   #
-  # - folder_name   (string)   "simu_results/single_c2/CPGM"
-  # - n             (integer)  
-  # - est_only      (boolean)  if true, only display est over time with ground truth 
+  # - graph_results_i   (list of all step_X's)  --> (list of all y_c_queries) --> (list of 'est' or 'truth' or 'coarse_truth' etc)
+  # - graph_ids         (vector of strings)    which graphs do we want?
+  # - i
+  # - j
+  # - full
+  #
   # 
   # output:
   #
-  #
+  # - graphs   (list)  list of graphs
   # 
   # ---------------------------------------------------------------------------- 
   
   
   # 1) load
   
-  graph_results_i <- get_file_name_and_load(folder_name, n) 
-  # graph_results_i <- old_to_new_graph_results_i(graph_results_i)
+
+  # step_0 <- step_list$step_0 
+  # step_1 <- step_list$step_1 
+  # step_2 <- graph_results_i$step_2
+  # step_3 <- graph_results_i$step_3
+  # step_4 <- graph_results_i$step_4
+  # step_5 <- graph_results_i$step_5
+  # step_6 <- graph_results_i$step_6
+  # step_7 <- graph_results_i$step_7
+  # step_8 <- graph_results_i$step_8 
+  # step_9 <- graph_results_i$step_9
+  # step_10 <- graph_results_i$step_10
+  # step_11 <- graph_results_i$step_11
+  # step_12 <- graph_results_i$step_12 
+  # step_2b <- graph_results_i$step_2b
+  
+  # graph_results_i also contains time_grid, time_grid_est, y_c_query
+  list2env(graph_results_i, envir = environment())  # does all of the above in one go
   
   # prep
   arr_mat_6 <- matrix(1:6, nrow = 2, byrow = F)
   arr_mat_8 <- matrix(1:8, nrow = 2, byrow = F)
   arr_mat_10 <- matrix(1:10, nrow = 2, byrow = F)  
   
+  m <- length(time_grid)
+  m_est <- length(time_grid_est)
   
   graphs <- list()
   
@@ -113,12 +158,15 @@ visualize_over_time <- function(folder_name, n, est_only, graph_id, m_est, m, i,
   # 2) figure out which steps have unique results for y_c_query and which steps are constant
   
   if('11' %in% graph_ids){
-    #   - X_k_est                   (p x m_est   x n)
-    #   - X_k_truth                 (p x m_truth x n)
-    #   - X_k_coarse_truth          (p x m_est   x n)
-    #   - X_k_both_truth            (p x m_both  x n)
-    #   - Lambda_k_truth            (p x m_truth x n)
-    #   - Lambda_k_coarse_truth     (p x m_est   x n)
+    
+    if(! full){
+      graphs[['g_11']] <- grid.arrange(visualize_log_intensity(step_1$X_k_est[1:5,,1],            time_grid_est,  'Estimate',       step_1$mu_t_coarse_truth ),
+                                       visualize_log_intensity(step_1$X_k_coarse_truth[1:5,,1],   time_grid_est,  'Coarser Truth',  step_1$mu_t_coarse_truth ),
+                                       visualize_log_intensity(step_1$X_k_truth[1:5,,1],          time_grid,      'Finer Truth',    step_1$mu_t_truth),
+                                       visualize_log_intensity(step_1$X_k_both_truth[1:5,,1],     time_grid_both, 'Combined Truth', step_1$mu_t_both_truth),
+                                       textGrob("0. Log Intensity\n of first 5 processes\nof subject 1", gp = gpar(fontsize = 14)),
+                                       layout_matrix = arr_mat_6) 
+    }
     
     step_1 <- graph_results_i$step_1
     
@@ -132,15 +180,74 @@ visualize_over_time <- function(folder_name, n, est_only, graph_id, m_est, m, i,
                                      visualize_log_intensity(step_1[[4]][1:5,,1],   time_grid_both, 'Combined Truth', mu_t_both),
                                      textGrob("0. Log Intensity\n of first 5 processes\nof subject 1", gp = gpar(fontsize = 14)),
                                      layout_matrix = arr_mat) 
+  }
+  
+  if('12' %in% graph_ids){
+    
+    if(! full){
+      
+      # p x m x n --> mean --> p x m --> choose first 5 processes --> 5 x m
+      
+      graphs[['g_12']] <- grid.arrange(visualize_log_intensity(apply(step_1$X_k_est,          c(1, 2), mean)[1:5, ],   time_grid_est,  'Estimate',       step_1$mu_t_coarse_truth ),
+                                       visualize_log_intensity(apply(step_1$X_k_coarse_truth, c(1, 2), mean)[1:5, ],   time_grid_est,  'Coarser Truth',  step_1$mu_t_coarse_truth ),
+                                       visualize_log_intensity(apply(step_1$X_k_truth,        c(1, 2), mean)[1:5, ],   time_grid,      'Finer Truth',    step_1$mu_t_truth),
+                                       visualize_log_intensity(apply(step_1$X_k_both_truth,   c(1, 2), mean)[1:5, ],   time_grid_both, 'Combined Truth', step_1$mu_t_both_truth),
+                                       textGrob("0. Average Log Intensity\n of first 5 processes", gp = gpar(fontsize = 14)),
+                                       layout_matrix = arr_mat_6) 
+    }
+  }
+  
+  if('22' %in% graph_ids){
+    g_list <- lapply(step_2, function(x){result_22_prep(x, time_grid, time_grid_est, full)})
+    graphs[['g_22']] <- rearrange_plots(g_list)
+  }
+  
+  # rho_ij(s,t) for process pair 1_1
+  if('24' %in% graph_ids){ 
+    g_list <- lapply(step_2b, function(x){result_20s_prep(x, i = 1, j = 1, full)})
+    graphs[['g_24']] <- rearrange_plots(g_list)
   }  
+  
+  # rho_ij(s,t) for process pair 1_2
+  if('25' %in% graph_ids){ 
+    g_list <- lapply(step_2b, function(x){result_20s_prep(x, i = 1, j = 2, full)})
+    graphs[['g_25']] <- rearrange_plots(g_list)
+  }  
+  
+  # rho_ij(s,t) for process pair 3_5
+  if('26' %in% graph_ids){ 
+    g_list <- lapply(step_2b, function(x){result_20s_prep(x, i = 3, j = 5, full)})
+    graphs[['g_26']] <- rearrange_plots(g_list)
+  }   
   
   # weights, all y_c_query settings in a row
   
   if('29' %in% graph_ids){
-    graph_list <- lapply(graph_results_i$step_2, result_29) 
+    
+    graph_list <- lapply(seq_along(step_2), function(i) {
+      result_29(step_2[[i]], y_c_names[i])
+    })
     
     graphs[['g_29']] <- do.call(grid.arrange, c(graph_list, nrow = 1))
   }
+  
+  # g_ij(s,t) at 1_1
+  if('31' %in% graph_ids){ 
+    g_list <- lapply(step_3, function(x){result_30s_prep(x, i = 1, j = 1, full)})
+    graphs[['g_31']] <- rearrange_plots(g_list)
+  }  
+  
+  # g_ij(s,t) at 1_2
+  if('32' %in% graph_ids){ 
+    g_list <- lapply(step_3, function(x){result_30s_prep(x, i = 1, j = 2, full)})
+    graphs[['g_32']] <- rearrange_plots(g_list)
+  }  
+  
+  # g_ij(s,t) at 3_5
+  if('33' %in% graph_ids){ 
+    g_list <- lapply(step_3, function(x){result_30s_prep(x, i = 3, j = 5, full)})
+    graphs[['g_33']] <- rearrange_plots(g_list)
+  }    
   
   if('81' %in% graph_ids & 'step_8' %in% names(graph_results_i)){
     est_graphs <- lapply(graph_results_i$step_8, function(x) extract_block_structure_ij(x$step_8$V_cond_est_full, m_est, i, j))
@@ -149,7 +256,7 @@ visualize_over_time <- function(folder_name, n, est_only, graph_id, m_est, m, i,
   # C_X1_X2
   
   if('91' %in% graph_ids){
-    graph_list <- lapply(graph_results_i$step_9, function(x){ result_90s_prep(x, m, m_est, i = 1, j = 2) })
+    graph_list <- lapply(graph_results_i$step_9, function(x){ result_90s_prep_ij(x, m, m_est, i = 1, j = 2, full) })
     
     n_y_c_query <- length(graph_list)
     n_settings <- length(graph_list[[1]])
@@ -173,7 +280,7 @@ visualize_over_time <- function(folder_name, n, est_only, graph_id, m_est, m, i,
   # C_X1_X2
   
   if('92' %in% graph_ids){
-    graph_list <- lapply(graph_results_i$step_9, function(x){ result_90s_prep(x, m, m_est, i = 3, j = 5) })
+    graph_list <- lapply(graph_results_i$step_9, function(x){ result_90s_prep_ij(x, m, m_est, i = 3, j = 5, full) })
     
     n_y_c_query <- length(graph_list)
     n_settings <- length(graph_list[[1]])
