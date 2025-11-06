7e9ff287e537628aec6c37775ba8c6a34b910d72
JASEN ZHANG <jasenzz@n6366.hoffman2.idre.ucla.edu>
2025-11-06 15:01:04 -0800

    hs norm with different spacings

--- Conditional_LGCP/functions/00b_matrix_norms.R
+++ Conditional_LGCP/functions/00b_matrix_norms.R
@@ -1,3 +1,5 @@
+library(akima)
+
 hilbert_schmidt_norm <- function(A) {
   
   # ------------------------------------------------------------------------
@@ -128,5 +130,58 @@ hilbert_schmidt_norm_pm_normalize <- function(A, p, m) {
   }
   
   return(delta_t * norms)
+  
+}
+
+hilbert_schmidt_norm_diff <- function(mat_A, mat_B, time_grid_A, time_grid_B, fine_n = 200){
+  
+  # ----------------------------------------------------------------------------
+  #
+  # GOAL: Calculate the Hilbert Schmidt norm of two matrices with different discretizations
+  #
+  # - we partition their grids finely so that they are the same dimensions.
+  #
+  # inputs:
+  #
+  # - mat_A         (m1 x m1 matrix)   first matrix of values
+  # - mat_B         (m2 x m2 matrix)   second matrix of values
+  # - time_grid_A   (m1-dim vec)       time discretization wrt first matrix
+  # - time_grid_B   (m2-dim vec)       time_discretization wrt second matrix
+  # - fine_n        (scalar)           how many timepoints we want to approximate the integral with
+  # 
+  # outputs:
+  #
+  # - HS_norm      (scalar)          HS norm of their differences
+  # 
+  #
+  # ----------------------------------------------------------------------------
+  
+  if (!requireNamespace("akima", quietly = TRUE)) {
+    stop("Please install the 'akima' package: install.packages('akima')")
+  }
+  
+  # Create coarse (x,y,z) data for A
+  grid_A <- expand.grid(x = time_grid_A, y = time_grid_A)
+  z_A <- as.vector(mat_A)
+  
+  # Create coarse (x,y,z) data for B
+  grid_B <- expand.grid(x = time_grid_B, y = time_grid_B)
+  z_B <- as.vector(mat_B)
+  
+  # Define fine grid (common resolution)
+  time_fine <- seq(0, 1, length.out = fine_n)
+  
+  # Bilinear interpolation onto fine grid
+  interp_A <- akima::interp(x = grid_A$x, y = grid_A$y, z = z_A,
+                            xo = time_fine, yo = time_fine, linear = TRUE, extrap = TRUE)
+  interp_B <- akima::interp(x = grid_B$x, y = grid_B$y, z = z_B,
+                            xo = time_fine, yo = time_fine, linear = TRUE, extrap = TRUE)
+  
+  # Approximate integral via Riemann sum
+  dx <- diff(time_fine)[1]
+  diff_sq <- (interp_A$z - interp_B$z)^2
+  HS_norm <- sqrt(sum(diff_sq, na.rm = TRUE) * dx^2)
+  
+  return(HS_norm)
   
 }
