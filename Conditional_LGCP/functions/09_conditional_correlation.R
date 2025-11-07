5f6c3620ef6a9f373527a31b769e882831ce9d37
JASEN ZHANG <jasenzz@n1828.hoffman2.idre.ucla.edu>
2025-11-07 15:48:28 -0800

    assuming eigenfunctions are not normalized

--- Conditional_LGCP/functions/09_conditional_correlation.R
+++ Conditional_LGCP/functions/09_conditional_correlation.R
@@ -55,96 +55,6 @@ estimate_conditional_correlation_v4 <- function(V_cond_mat, p, identity = T, pin
   return(C_conditional)
 }
 
-
-
-correlation_estimation_KL_cov <- function(eigendecomp, KL_cov, identity = T){
-  
-  # ----------------------------------------------------------------------------
-  # 
-  #
-  # GOAL: construct the correlation operator from the KL covariance method
-  #
-  # input:
-  #
-  # - eigendecomp (list of 3 entries)
-  #
-  #   - eigenvalues
-  #   - eigenfunctions
-  #   - n_dims
-  #
-  # - KL_cov (list of i_j entries, each is d x d matrix) each value represents covariance between KL coeffs of components a and b in process i and j
-  # - identity    (boolean)  are the i = j entries just the identity?
-  #
-  # output:
-  #
-  # - C_cond   (list of i_j entries)
-  #
-  #
-  # ----------------------------------------------------------------------------
-  
-  
-  p <- length(eigendecomp[[1]])
-  d <- dim(KL_cov[[1]])[1]
-  m <- dim(eigendecomp[[2]][[1]])[1]
-  Delta <- 1/m
-  
-  C_cond <- list()
-  
-  for(i in 1:p){
-    for(j in i:p){
-      
-      key <- paste0(i, '_', j)
-      
-      if(i == j & identity){
-        C_ij <- diag(m)
-      } else{
-        
-      
-        
-        
-        eval_i <- eigendecomp[[1]][[i]]
-        eval_j <- eigendecomp[[1]][[j]]
-        
-        evec_i <- eigendecomp[[2]][[i]]
-        evec_j <- eigendecomp[[2]][[j]]
-        
-        d_i <- dim(evec_i)[2]
-        d_j <- dim(evec_j)[2]
-        
-        cov_ij <- KL_cov[[key]]
-        
-        
-        # cov / sqrt(var_i * var_j)
-        coeffs <- cov_ij / sqrt(tcrossprod(eval_i, eval_j))
-        
-        if (any(is.nan(coeffs))) {
-          stop("correlation construction process contains NaN values!")
-        }      
-        
-        
-        C_ij <- matrix(0, nrow = m, ncol = m)
-        
-        for(a in 1:d_i){
-          for(b in 1:d_j){
-            
-            # REMEMBER, we could have eigenvectors defined as Delta * (v^\top v) = 1. 
-            
-            C_ij <- C_ij + coeffs[a,b] * tcrossprod(evec_i[, a], evec_j[, b])
-            
-          }
-        }
-      } # end of nonidentity case 
-      
-      C_cond[[key]] <- C_ij
-      
-    }
-  }
-  
-  return(C_cond)
-  
-  
-}
-
 correlation_estimation_KL_cor <- function(eigendecomp, KL_cor, identity = T){
   
   # ----------------------------------------------------------------------------
@@ -157,7 +67,7 @@ correlation_estimation_KL_cor <- function(eigendecomp, KL_cor, identity = T){
   # - eigendecomp (list of 3 entries)
   #
   #   - eigenvalues
-  #   - eigenfunctions
+  #   - eigenfunctions   NOT NORMALIZED
   #   - n_dims
   #
   # - KL_cor      (list of i_j entries, each is d x d matrix) each value represents correlation between KL coeffs of components a and b in process i and j
@@ -206,9 +116,9 @@ correlation_estimation_KL_cor <- function(eigendecomp, KL_cor, identity = T){
         for(a in 1:d_i){
           for(b in 1:d_j){
             
-            # REMEMBER, we could have eigenvectors defined as Delta * (v^\top v) = 1. 
+            # REMEMBER, EIGENFUNCTIONS ARE NOT NORMALIZED
             
-            C_ij <- C_ij + cor_ij[a,b] * tcrossprod(evec_i[, a], evec_j[, b])
+            C_ij <- C_ij + cor_ij[a,b] * Delta^2 * tcrossprod(evec_i[, a], evec_j[, b])
             
           }
         }
