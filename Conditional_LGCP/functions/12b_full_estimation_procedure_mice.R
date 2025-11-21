56f7f19b54f97c3c92b5649710a85b6aded6fd70
JASEN ZHANG <jasenzz@n1829.hoffman2.idre.ucla.edu>
2025-11-20 16:59:20 -0800

    typo

--- Conditional_LGCP/functions/12b_full_estimation_procedure_mice.R
+++ Conditional_LGCP/functions/12b_full_estimation_procedure_mice.R
@@ -620,8 +620,8 @@ estimate_intensities_stratum_parallel_with_yc_part3 <- function(temp_file_dir, s
     rho_i_file_names  <- paste0('step_2_rho_i_',  ID, '_', discrete_level, '_t', time_scale, '_nquery', cont_ind, '_', 1:n_keys_univariate, '.rds')
     rho_ij_file_names <- paste0('step_2_rho_ij_', ID, '_', discrete_level, '_t', time_scale, '_nquery', cont_ind, '_', 1:n_keys_bivariate, '.rds')    
   } else{
-    rho_i_file_name  <- paste0("step_2_rho_i_",  adj_type, '_n_', n, '_nquery', cont_ind, '_', 1:n_keys_univariate, '.rds')
-    rho_ij_file_name <- paste0("step_2_rho_ij_", adj_type, '_n_', n, '_nquery', cont_ind, '_', 1:n_keys_bivariate, '.rds')
+    rho_i_file_names  <- paste0("step_2_rho_i_",  adj_type, '_n_', n, '_nquery', cont_ind, '_', 1:n_keys_univariate, '.rds')
+    rho_ij_file_names <- paste0("step_2_rho_ij_", adj_type, '_n_', n, '_nquery', cont_ind, '_', 1:n_keys_bivariate, '.rds')
   }
 
   
