9063e6704aa810163382a11cf0e83ca12db6278e
JASEN ZHANG <jasenzz@n1961.hoffman2.idre.ucla.edu>
2025-11-24 22:12:34 -0800

    run

--- Conditional_LGCP/functions/26_complete_data_generation_pipeline.R
+++ Conditional_LGCP/functions/26_complete_data_generation_pipeline.R
@@ -988,7 +988,7 @@ simulate_finite_basis_cox_data_part5 <- function(temp_file_dir, setting_info_lis
   part1_file_name          <- paste0('part1_', adj_type, '_n_', n, '_group', 1:group_nums, '.rds')
   events_file_name         <- paste0('events_', adj_type, '_n_', n, '_group', 1:group_nums, '.rds')
   truths_file_name         <- paste0('truths_', adj_type, '_n_', n, '_nquery', 1:cont_inds, '.rds')
-  dataset_file_name        <- paste0('dataset', adj_type, '_n_', n, '.rds')
+  dataset_file_name        <- paste0('dataset_', adj_type, '_n_', n, '.rds')
   
   file.remove(file.path(temp_file_dir, part0_file_name)) 
   file.remove(file.path(temp_file_dir, part1_file_name)) 
--- Conditional_LGCP/scripts/gen_fit_finite_basis_data_v3.sh
+++ Conditional_LGCP/scripts/gen_fit_finite_basis_data_v3.sh
@@ -25,8 +25,8 @@ adj_type_params=(
   "block_banded_c0 0.5 0.5 2"
 )
 
-n_large=500
-ns=(500)
+n_large=5000
+ns=(1000 3000 5000)
 n_group=10
 groups=$(( n_large / n_group ))
 method="CPGM"
