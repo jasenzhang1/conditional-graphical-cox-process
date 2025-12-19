ea1ed03ef3a45e5203c83210229af946190bf56a
JASEN ZHANG <jasenzz@n1829.hoffman2.idre.ucla.edu>
2025-12-19 10:41:56 -0800

    112b part

--- Conditional_LGCP/functions/28c_Grand_Visualizations.R
+++ Conditional_LGCP/functions/28c_Grand_Visualizations.R
@@ -356,6 +356,15 @@ visualize_over_time <- function(graph_results_i, graph_ids, beta_truth, X_truth)
     graphs[['g_112']] <- result_heatmap_nonblock_prep(step_11, 'w_mat', time_grid_est, data_format = 'regular', rm_diag = T, zmid = 0)
   }  
   
+  # P_HS (w_mat) for all pxp blocks - delete est and X_truth
+  if('112' %in% graph_ids){
+    step_11_v2 <- step_11
+    step_11_v2 <- lapply(step_11_v2, function(x) {
+      x[!names(x) %in% c("w_mat_est", "w_mat_X_truth")]
+    })
+    graphs[['g_112b']] <- result_heatmap_nonblock_prep(step_11_v2, 'w_mat', time_grid_est, data_format = 'regular', rm_diag = T, zmid = 0)
+  } 
+  
   # ROC curves
   if('113' %in% graph_ids){
     
