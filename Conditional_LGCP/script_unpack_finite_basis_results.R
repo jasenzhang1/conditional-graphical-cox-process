e4a43992e606ce8b80af41eef49ed262206ef01a
JASEN ZHANG <jasenzz@n1828.hoffman2.idre.ucla.edu>
2026-01-08 11:54:45 -0800

    wip

--- Conditional_LGCP/script_unpack_finite_basis_results.R
+++ Conditional_LGCP/script_unpack_finite_basis_results.R
@@ -1,4 +1,7 @@
-# unpack
+# main script to unpack finite basis results
+
+# 1) packages and functions 
+
 library(grid)
 library(gridExtra)
 library(dplyr)
@@ -12,24 +15,32 @@ source('functions/28c_Grand_Visualizations.R')
 source('functions/00d_debugging.R')
 source('functions/28e_Finite_Basis_Visualization.R')
 
+# 2) arguments for simulation settings
+
 args <- commandArgs(trailingOnly = TRUE)
 
 data_folder <- args[1]    # data_folder <- 'simu_data'
 base_folder <- args[2]    # base_folder <- 'simu_results'
 adj_type    <- args[3]    # adj_type <- 'block_banded_v2'
 method      <- args[4]    # method <- 'CPGM'
+X_truth     <- args[5]
+beta_truth  <- args[6]
 
 # data_folder <- 'simu_data'
 # base_folder <- 'simu_results'
 # adj_type <- 'flexible_block_banded_c0'
 # method <- 'CPGM'
+# X_truth <- T
+# beta_truth <- T
 
-n_large <- 600
-n <- 600
 
-# heatmaps of certain metrics 
+n_large <- 3000
+n <- 3000
 
-graph_ids <- c('22',    # rho_i
+# 3) heatmaps of certain metrics 
+
+graph_ids <- c('11', '12',
+               '22',    # rho_i
                '25',    # rho_ij [1,2]
                '32',    # g_ij [1,2]
                '112',   # w_mat  (P_HS)
@@ -39,9 +50,10 @@ truth_file_name <- paste0(data_folder, '/', adj_type, '_n_', n_large, '_truths.R
 estimates_file_name <- paste0(base_folder, '/', adj_type, '/', method, '/', adj_type, '_n_', n, '.RData')
 
 # 28e - visualize results over y_c
-g_heatmaps <- visualize_finite_basis(truth_file_name, estimates_file_name, graph_ids)
+g_heatmaps <- visualize_finite_basis(truth_file_name, estimates_file_name, graph_ids, beta_truth, X_truth)
 
-# 2) convergence of intermediate estimators (28b)
+# ------------------------------------------------------------------------------
+# 4) convergence of intermediate estimators (28b)
 
 i <- 1
 j <- 2
