fe7df8b478484a6b63c1d0b4edd3757b86418846
JASEN ZHANG <jasenzz@n1801.hoffman2.idre.ucla.edu>
2026-01-27 17:54:09 -0800

    eigen_setting argument

--- Conditional_LGCP/script_fit_mice_data_part2b.R
+++ Conditional_LGCP/script_fit_mice_data_part2b.R
@@ -21,6 +21,9 @@ if(model_type == 'mice'){
   movement <- as.numeric(args[6])     # movement <- 0
   VR <- as.numeric(args[7])           # VR <- 0
   cont_ind <- as.numeric(args[8])     # cont_ind <- 1  
+  eigen_setting <- args[9]
+  
+  X_truth <- F
   
   discrete_level <- paste0('m', movement, 'vr', VR)
   
@@ -41,22 +44,12 @@ if(model_type == 'mice'){
   method <- args[5]
   X_truth <- as.logical(args[6])
   cont_ind <- as.numeric(args[7])
-  same_basis <- as.logical(args[8])   # do we use the same basis for each process for eigendecomposition?
-  raw_d <- args[9]   # do we use the same amount of eigencomponents for each process? If so, which number. either a number or null
-    
-  # 2. Check if it's the string "NULL", otherwise convert to numeric
-  if (raw_d == "NULL" || is.na(raw_d)) {
-    constant_d <- NULL
-  } else {
-    constant_d <- as.numeric(raw_d)
-  }
+  eigen_setting <- args[8]
   
   setting_info_list <- list(n_large = n_large,
                             n = n,
                             adj_type = adj_type,
-                            method = method,
-                            same_basis = same_basis,
-                            constant_d = constant_d)
+                            method = method)
   
   temp_file_dir <- 'temp_data/simu'
   mouse <- F
@@ -70,7 +63,7 @@ if(model_type == 'mice'){
 # estimation - create file called 'part_3....rds' in /temp_data/simu
 # ---------------------------
 
-full_conditional_estimation_with_no_truth_part2b(temp_file_dir, setting_info_list, cont_ind, mouse, X_truth)
+full_conditional_estimation_with_no_truth_part2b(temp_file_dir, setting_info_list, cont_ind, mouse, X_truth, eigen_setting)
 
 
 
