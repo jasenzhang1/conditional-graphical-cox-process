4815d171b6e27e9bfc36c0ac2266433c294d66c3
JASEN ZHANG <jasenzz@n1826.hoffman2.idre.ucla.edu>
2026-02-05 12:16:23 -0800

    added all the visualization functions

--- Conditional_LGCP/functions/20_simulation_function_wrapper.R
+++ Conditional_LGCP/functions/20_simulation_function_wrapper.R
@@ -1,12 +1,17 @@
 
 
-packages <- c("dplyr", "tidyr", "data.table", "gridExtra", "grid", "patchwork",
+packages <- c("dplyr", "tidyr", "knitr",
+              "data.table", "gridExtra", "grid", "ggplot2", "patchwork",
               "Matrix", "mvtnorm", "igraph", "MASS", "abind", "reshape2",
-              "ggplot2", "parallel", "purrr", "pracma")
+              "parallel", "purrr", "pracma")
 
 for (p in packages) {
   suppressPackageStartupMessages(library(p, character.only = TRUE))
 }
+
+source('functions/00a_matrix_massaging.R')
+source('functions/00b_matrix_norms.R')
+source('functions/00d_debugging.R')
 
 source('functions/21_graph_structure_generation.R')
 source('functions/22_generate_random_variables.R')
@@ -16,10 +21,14 @@ source('functions/24a_precision_operator_from_E_yc_yd.R')
 source('functions/25_point_generation_process.R')
 source('functions/26_complete_data_generation_pipeline.R')
 source('functions/27_performance_evaluation_functions.R')
+
 source('functions/28_Simulation_Visualization.R')
 source('functions/28b_Simulation_Visualization_2.R')
-source('functions/00a_matrix_massaging.R')
-source('functions/00b_matrix_norms.R')
+source('functions/28c_Grand_Visualizations.R')
+source('functions/28e_Finite_Basis_Visualization.R')
+source('functions/28y_Visualization_Blocks.R')
+source('functions/28z_Visualization_helpers.R')
+
 
 
 
