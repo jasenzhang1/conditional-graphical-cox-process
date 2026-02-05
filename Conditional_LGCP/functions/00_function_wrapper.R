5a5ed511639cec4bed3cc653da89e74a4501ab08
JASEN ZHANG <jasenzz@n1829.hoffman2.idre.ucla.edu>
2026-02-04 17:01:41 -0800

    packages

--- Conditional_LGCP/functions/00_function_wrapper.R
+++ Conditional_LGCP/functions/00_function_wrapper.R
@@ -1,5 +1,6 @@
 packages <- c("dplyr", "data.table", "pROC", "grid", "abind", "future.apply", "pbmcapply",
-              "Matrix", "ggplot2", "reshape2", "gridExtra", "viridis", "MASS", 'igraph')
+              "Matrix", "ggplot2", "reshape2", "gridExtra", "viridis", "MASS", 'igraph',
+              'knitr', 'tidyr', 'rmarkdown')
 
 for (package in packages) {
   suppressPackageStartupMessages(library(package, character.only = TRUE))
