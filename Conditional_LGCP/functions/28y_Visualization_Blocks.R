aab2f8c418536bfa0a3e3372bf213d87cca85b52
JASEN ZHANG <jasenzz@n7643.hoffman2.idre.ucla.edu>
2026-02-13 15:48:33 -0800

    ROC curve allows for blank space for null graphs

--- Conditional_LGCP/functions/28y_Visualization_Blocks.R
+++ Conditional_LGCP/functions/28y_Visualization_Blocks.R
@@ -607,10 +607,15 @@ result_histogram_prep <- function(my_list, entry_name, data_format, nbins = 20){
   
 }
 
-result_ROC_prep <- function(step_12, kept_suffix, kept_time_idx){
+result_ROC_prep <- function(step_12, kept_names, kept_time_idx){
   
-  if(! is.null(kept_suffix)){
-    kept_names <- paste0('roc_', kept_suffix)
+  # 
+  # kept_names = which items to keep
+  #
+  # kept_time_idx = c(2,3) out of c(1,2,3,4), if we want to display certain weeks
+  #
+  
+  if(! is.null(kept_names)){
     
     step_12 <- lapply(step_12, function(x) {
       x[names(x) %in% kept_names]
@@ -637,21 +642,35 @@ result_ROC_prep <- function(step_12, kept_suffix, kept_time_idx){
       ideal_threshold <- roc_entry$threshold
       auc_value       <- roc_entry$auc
       
-      p <- ggplot(roc_df, aes(x = FPR, y = TPR)) +
-        geom_step(direction = "vh", color = "blue", size = 1) +
-        geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "grey") +
-        labs(
-          title = paste('y=', y_names[i], ', ', x_names[j]),
-          x = "False Positive Rate", y = "True Positive Rate"
-        ) +
-        annotate("point", x = 1 - ideal_spec, y = ideal_sens, color = "red", size = 3) +
-        annotate("text", x = 1 - ideal_spec, y = ideal_sens,
-                 label = paste0("thr=", round(ideal_threshold, 3)),
-                 hjust = -0.1, vjust = -0.5, color = "red") +
-        annotate("text", x = 0.6, y = 0.2,
-                 label = paste0("AUC=", round(auc_value, 3)),
-                 color = "darkgreen", size = 5) +
-        theme_minimal(base_size = 10)
+      plot_title <- paste('y=', y_names[i], ', ', x_names[j])
+      
+      if(is.data.frame(roc_df)){
+        p <- ggplot(roc_df, aes(x = FPR, y = TPR)) +
+          geom_step(direction = "vh", color = "blue", size = 1) +
+          geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "grey") +
+          labs(
+            title = plot_title,
+            x = "False Positive Rate", y = "True Positive Rate"
+          ) +
+          annotate("point", x = 1 - ideal_spec, y = ideal_sens, color = "red", size = 3) +
+          annotate("text", x = 1 - ideal_spec, y = ideal_sens,
+                   label = paste0("thr=", round(ideal_threshold, 3)),
+                   hjust = -0.1, vjust = -0.5, color = "red") +
+          annotate("text", x = 0.6, y = 0.2,
+                   label = paste0("AUC=", round(auc_value, 3)),
+                   color = "darkgreen", size = 5) +
+          theme_minimal(base_size = 10) +
+          theme(
+            plot.title = element_text(size = 8, face = "bold", hjust = 0.5)
+          )
+      } else {
+        p <- ggplot() + 
+          ggtitle(plot_title) + 
+          theme_void() + 
+          theme(
+            plot.title = element_text(size = 8, face = "bold", hjust = 0.5, vjust = 1)
+          )
+      }
       
       all_plots[[length(all_plots) + 1]] <- p
     }
