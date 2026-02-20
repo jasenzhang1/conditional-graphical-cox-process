628edfa9f1574f3fc90a8f675bbe7ca40deba149
JASEN ZHANG <jasenzz@n1962.hoffman2.idre.ucla.edu>
2026-02-20 11:32:01 -0800

    add ground_truth argument

--- Conditional_LGCP/functions/28e_Finite_Basis_Visualization.R
+++ Conditional_LGCP/functions/28e_Finite_Basis_Visualization.R
@@ -3,7 +3,7 @@ source('functions/28y_Visualization_Blocks.R')
 source('functions/28_Simulation_Visualization.R')
 source('functions/28c_Grand_Visualizations.R')
 
-visualize_finite_basis <- function(truth_file_name, estimates_file_name, graph_ids, beta_truth, X_truth, eigen_troubleshoot){
+visualize_finite_basis <- function(truth_file_name, estimates_file_name, graph_ids, ground_truth, beta_truth, X_truth, eigen_troubleshoot){
   
   # ----------------------------------------------------------------------------
   #
@@ -19,6 +19,7 @@ visualize_finite_basis <- function(truth_file_name, estimates_file_name, graph_i
   # - graph_ids             (vector of strings)   which graphs do we want to see
   # - i
   # - j
+  # - ground_truth          (boolean)             do our results have the overall truth?
   # - beta_truth            (boolean)             do our results have beta_truth values? 
   # - X_truth               (boolean)             do our results have X_truth values? 
   # - eigen_troubleshoot    (boolean)             does step_4 have eig1 eig2 eig3 suffixes?
@@ -39,7 +40,7 @@ visualize_finite_basis <- function(truth_file_name, estimates_file_name, graph_i
 
   # 2) now, utilize the code in 28c
 
-  g_comparisons <- visualize_over_time(merged, graph_ids, beta_truth, X_truth, eigen_troubleshoot)
+  g_comparisons <- visualize_over_time(merged, graph_ids, ground_truth, beta_truth, X_truth, eigen_troubleshoot)
   
   return(g_comparisons)
   
