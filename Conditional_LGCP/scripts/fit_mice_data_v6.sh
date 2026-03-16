e7b4e70a6414c235609ea38c6d8516b23b85bdec
JASEN ZHANG <jasenzz@n6167.hoffman2.idre.ucla.edu>
2026-03-15 23:34:09 -0700

    run

--- Conditional_LGCP/scripts/fit_mice_data_v6.sh
+++ Conditional_LGCP/scripts/fit_mice_data_v6.sh
@@ -29,7 +29,7 @@
 cd "$(dirname "$0")/.."   # go one level up (from /scripts to /)
 
 
-IDs=("Tau3" "WT3")
+IDs=("Tau1" "Tau2" "WT1" "WT2")
 y_c_structure="week_only"
 method="CPGM"
 model_type="mice"  # simu or mice
