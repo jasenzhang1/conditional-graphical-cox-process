9c6fb22c11c56380c000be75396bd45cea527464
JASEN ZHANG <jasenzz@n6643.hoffman2.idre.ucla.edu>
2026-05-04 10:48:06 -0700

    run

--- Conditional_LGCP/script_unpack_mice_results_graph_over_time.R
+++ Conditional_LGCP/script_unpack_mice_results_graph_over_time.R
@@ -10,7 +10,7 @@ source('functions/20_simulation_function_wrapper.R')
 # for all 4 discrete settings, group them and plot adjacency matrices over time
 
 
-y_c_structure <- "week_only_bw_001"
+y_c_structure <- "week_only_bw_003_min_01_before_fix"
 method <- 'CPGM'
 region <- 'BOTH_150'  # HIP, EHC, BOTH_100, BOTH_150
 time_scale <- 10
