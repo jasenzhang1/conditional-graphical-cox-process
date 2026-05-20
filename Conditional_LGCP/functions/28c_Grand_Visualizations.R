93de6e037c836b4e41988f218f010cb8334f02fb
JASEN ZHANG <jasenzz@n1067.hoffman2.idre.ucla.edu>
2026-05-20 14:54:02 -0700

    vline

--- Conditional_LGCP/functions/28c_Grand_Visualizations.R
+++ Conditional_LGCP/functions/28c_Grand_Visualizations.R
@@ -1981,10 +1981,10 @@ visualize_strata_all_mice_v2 <- function(results_folder, time_scale, discrete_le
       g <- g +
         geom_vline(data = boundary_data,
                    aes(xintercept = boundary),
-                   color = "gray90", alpha = 1, size = 0.6) +
+                   color = "gray60", alpha = 1, size = 0.9) +
         geom_hline(data = boundary_data,
                    aes(yintercept = boundary),
-                   color = "gray90", alpha = 1, size = 0.6)
+                   color = "gray40", alpha = 1, size = 0.6)
     }
     
     # Layer 4: facet + formatting.
