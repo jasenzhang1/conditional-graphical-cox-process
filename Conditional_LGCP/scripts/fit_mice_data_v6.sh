6b04f34f48b207a4e2e47b0cba3e751b435d4d9d
JASEN ZHANG <jasenzz@n1038.hoffman2.idre.ucla.edu>
2026-05-21 00:41:16 -0700

    run

--- Conditional_LGCP/scripts/fit_mice_data_v6.sh
+++ Conditional_LGCP/scripts/fit_mice_data_v6.sh
@@ -734,7 +734,7 @@ else
             
             MICE_FOLDER="temp_data/mice/${ID}_m${mov}vr${vr}_t${time_scale}"
             
-            rm -rf "$MICE_FOLDER"
+            # rm -rf "$MICE_FOLDER"
             
             echo "Cleanup complete for $ID, m${mov}vr${vr}, t=$time_scale." >> "$outfile"
             
