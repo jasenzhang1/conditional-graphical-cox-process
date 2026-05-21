35525c89a5b3cba628244262abb22cf46ee8afcf
JASEN ZHANG <jasenzz@n1038.hoffman2.idre.ucla.edu>
2026-05-21 00:04:17 -0700

    multiple min connects

--- Conditional_LGCP/script_GIC_local_part2and3_serial.R
+++ Conditional_LGCP/script_GIC_local_part2and3_serial.R
@@ -16,7 +16,7 @@ if(model_type == 'mice'){
   cont_ind <- as.numeric(args[8])     # cont_ind <- 1  
   id_suffix <- as.numeric(args[9])    # est or X_truth
   k         <- as.numeric(args[10])   # tau_c index
-  min_connect_pct <- as.numeric(args[11])
+  min_connect_pcts <- as.numeric(args[11:length(args)])
   
   
   X_truth <- F
@@ -57,6 +57,6 @@ if(model_type == 'mice'){
 task_csv <- read.csv(paste0(temp_file_dir, '/task_map.csv'))
 
 suffix_name <- task_csv[id_suffix, 1]
-GIC_step2and3_serial_tau_c(temp_file_dir, suffix_name, k, min_connect_pct)
+GIC_step2and3_serial_tau_c(temp_file_dir, suffix_name, k, min_connect_pcts)
 
 
