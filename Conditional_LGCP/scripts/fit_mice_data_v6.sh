b587e30d7d8374af6cd84d1ccf0023ffbb0055d6
JASEN ZHANG <jasenzz@n1832.hoffman2.idre.ucla.edu>
2026-05-22 15:47:16 -0700

    run

--- Conditional_LGCP/scripts/fit_mice_data_v6.sh
+++ Conditional_LGCP/scripts/fit_mice_data_v6.sh
@@ -52,7 +52,7 @@ global_thresh_method="neither" # both, joint, tau_c, or neither
 y_c_structure="week_only"
 method="CPGM"
 min_connect_pcts=(0 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.10)    # 1 percent = 0.01
-min_connect_pcts=(0.03 0.04)    # 1 percent = 0.01
+min_connect_pcts=(0.13 0.25)    # 1 percent = 0.01
 
 m=30
 time_scale=10 
