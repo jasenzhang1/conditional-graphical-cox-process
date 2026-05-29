bfa4564e477e0030acc0f18013aaeacc68806b84
JASEN ZHANG <jasenzz@n1143.hoffman2.idre.ucla.edu>
2026-05-29 03:48:08 -0700

    typo

--- Conditional_LGCP/scripts/fit_mice_data_v6.sh
+++ Conditional_LGCP/scripts/fit_mice_data_v6.sh
@@ -97,7 +97,7 @@ else
     
 
     # I have done 0.001 and 0.0003, next try 0.003
-    region="BOTH_150"   # "HIP", "EHC", or "HIP_EHC" "BOTH_100", "BOTH_150", "BOTH_100_NORMALIZED"
+    region="BOTH_100_NORMALIZED"   # "HIP", "EHC", or "HIP_EHC" "BOTH_100", "BOTH_150", "BOTH_100_NORMALIZED"
     
     max_jobs=75
     
